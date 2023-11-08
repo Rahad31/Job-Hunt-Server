36e4643a92358ec6de70cf03cc01121a76534b79
Rahad31 <rahad3100@gmail.com>
2023-11-08 21:04:31 +0600

    jwt setup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
@@ -93,11 +95,22 @@ async function run() {
       res.send(result);
     });
 
-      // app.get("/appjob", async (req, res) => {
-      //   const cursor = appjobCollection.find();
-      //   const result = await cursor.toArray();
-      //   res.send(result);
-      // });
+    // auth related api
+    app.post("/jwt", logger, async (req, res) => {
+      const user = req.body;
+      console.log("user for token", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      // 
+    });
+
+    // app.get("/appjob", async (req, res) => {
+    //   const cursor = appjobCollection.find();
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
 
     // app.get("/job/:id", async (req, res) => {
     //   const id = req.params.id;
